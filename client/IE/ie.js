4f3a964e258f1819c2789756c264de9f658c500d
Laysa Siqueira <laysaasiqueiraa@gmail.com>
2025-04-24 21:52:39 +0100

    importação e exportação

---
+++ client/IE/ie.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const { Builder } = require('xml2js');
+
+// Caminhos absolutos dos arquivos
+const filePaths = [
+  path.resolve(__dirname, '../../server/graphql/data/students.json'),
+  path.resolve(__dirname, '../../server/graphql/data/lessons.json'),
+  path.resolve(__dirname, '../../server/grpc/data/students.json'),
+  path.resolve(__dirname, '../../server/rest/database/alunos.json')
+];
+
+// Função para ler JSON de um arquivo
+function readJson(filePath) {
+  const content = fs.readFileSync(filePath, 'utf8');
+  return JSON.parse(content);
+}
+
+// Combinar os dados de todos os arquivos
+let dadosCombinados = [];
+
+filePaths.forEach((filePath) => {
+  const data = readJson(filePath);
+  if (Array.isArray(data)) {
+    dadosCombinados = dadosCombinados.concat(data);
+  } else {
+    dadosCombinados.push(data);
+  }
+});
+
+// Escrever dados combinados em JSON
+fs.writeFileSync(
+  path.resolve(__dirname, 'dados_combinados.json'),
+  JSON.stringify(dadosCombinados, null, 2),
+  'utf8'
+);
+
+// Preparar dados para XML
+const xmlBuilder = new Builder();
+const xml = xmlBuilder.buildObject({ dados: { item: dadosCombinados } });
+
+fs.writeFileSync(
+  path.resolve(__dirname, 'dados_combinados.xml'),
+  xml,
+  'utf8'
+);
+
+console.log('Exportação para JSON e XML concluída!');
